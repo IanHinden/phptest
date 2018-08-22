39a5196d515825afcedc793a739beff82b59b3df
IanHinden <Ian.Hinden@gmail.com>
2018-08-21 18:42:52 -0700

    Added form submission

--- connect.php
+++ connect.php
@@ -1,30 +1,19 @@
-
+<!DOCTYPE html>
 <html>
 <head>
 <title>Connect</title>
 </head>
 <body>
 
-<?php
-//Connect To Database
-echo "Let's begin.";
-$hostname='phpmyadmin.clv11adojqww.us-west-2.rds.amazonaws.com';
-echo "Now log in.";
-$username='phpMyAdmin';
-$password='phpmyadmin';
-$dbname='testingnewdb';
-$usertable='users';
-echo "I am logged in. Now connect!";
+<form action="includes/signup.inc.php" method="POST">
+	<input type="text" name="first" placeholder="FirstName">
+	<br>
+	<input type="test" name="last" placeholder="LastName">
+	<br>
+	<input type="number" name="age" placeholder="Age">
+	<br>
+	<button type="submit" name="submit">Sign up!</button>
+</form>
 
-$con = mysqli_connect($hostname, $username, $password, $dbname);
-if ($con-> connect_error) {
-        die("Connection failed:" . $con-> connect_error);
-}
-echo "Connected properly";
-
-$sql = "INSERT INTO users (Number, First_Name, Last_Name, Age) VALUES (2, 'Don', 'Perrin', 50);";
-mysqli_query($con, $sql);
-mysqli_close($db_handle);
-?>
 </body>
 </html>
